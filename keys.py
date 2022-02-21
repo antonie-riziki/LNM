e976fafbf5aea77793fa50f2716b055ca0411c54
antonie <antonriziki@gmail.com>
2022-02-21 05:26:25 +0300

    second commit

---
+++ keys.py
@@ -0,0 +1,6 @@
+
+business_short_code = "174379"
+phone_number = "254743158232"
+lipa_na_mpesa_passkey = ""
+consumer_key = ""
+consumer_secret = ""
--- lipanampesa.py
+++ lipanampesa.py
@@ -1,1 +1,26 @@
-print('Lipa na mpesa')
+import requests
+import keys
+
+access_token = "access_token"
+
+api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+
+headers = { "Authorization": "Bearer %s" % access_token }
+
+request = {
+    "BusinessShortCode": keys.business_short_code,
+    "Password": "",
+    "Timestamp":"",
+    "TransactionType": "CustomerPayBillOnline",
+    "Amount":"1",
+    "PartyA":"",
+    "PartyB":"",
+    "PhoneNumber":"",
+    "CallBackURL":"https://mydomain.com/pat",
+    "AccountReference":"Lipa na Mpesa Test Push",
+    "TransactionDesc":""
+}
+
+response = requests.post(api_url, json = request, headers = headers)
+
+print(response.text)
