ae8c4d97dc1d38084fb77c94e0aed3ffc8977790
antonie <antonriziki@gmail.com>
2022-02-21 06:00:42 +0300

    third commit

--- lipanampesa.py
+++ lipanampesa.py
@@ -1,26 +1,43 @@
 import requests
 import keys
+import base64
 
-access_token = "access_token"
 
-api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+from datetime import datetime
 
-headers = { "Authorization": "Bearer %s" % access_token }
 
-request = {
-    "BusinessShortCode": keys.business_short_code,
-    "Password": "",
-    "Timestamp":"",
-    "TransactionType": "CustomerPayBillOnline",
-    "Amount":"1",
-    "PartyA":"",
-    "PartyB":"",
-    "PhoneNumber":"",
-    "CallBackURL":"https://mydomain.com/pat",
-    "AccountReference":"Lipa na Mpesa Test Push",
-    "TransactionDesc":""
-}
+ctime = datetime.now().strftime("%Y%m%d%H%M%S") # convert the current sys time to string using (strftime)
+print(ctime)
 
-response = requests.post(api_url, json = request, headers = headers)
+data_to_encode = keys.business_short_code + keys.lipa_na_mpesa_passkey + ctime # collection of parameters that make the password variable
+encode_data = base64.b64encode(data_to_encode.encode()) # encode the data to a base64 format
+password = encode_data.decode('utf-8') # change the byte-like to str by decoding
+print(password)
 
-print(response.text)
+
+def lipa_na_mpesa():
+    access_token = "access_token"
+
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+
+    headers = { "Authorization": "Bearer %s" % access_token }
+
+    request = {
+        "BusinessShortCode": keys.business_short_code,
+        "Password": password,  # is a base64 encoded utf-8 string format consisting of Shortcode+Passkey+Timestamp
+        "Timestamp": ctime,  # current system date + time
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount":"1",
+        "PartyA": keys.phone_number,
+        "PartyB": keys.business_short_code,
+        "PhoneNumber": keys.phone_number,
+        "CallBackURL":"https://mydomain.com/pat",
+        "AccountReference":"Lipa na Mpesa Test Push",
+        "TransactionDesc":""
+    }
+
+    response = requests.post(api_url, json = request, headers = headers)
+
+    print(response.text)
+
+#lipa_na_mpesa()
