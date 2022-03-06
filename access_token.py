887e66ffb43b7e63f1c345bdbadace5887973228
antonie <antonriziki@gmail.com>
2022-03-06 18:31:53 +0300

    code refactoring - access token

--- access_token.py
+++ access_token.py
@@ -11,5 +11,6 @@ def access_token_generator():
     r = requests.get(api_url, auth=HTTPBasicAuth(keys.consumer_key, keys.consumer_secret))
     json_access_token = r.json()
     generated_access_token = json_access_token['access_token']
+    print(generated_access_token)
 
     return generated_access_token
